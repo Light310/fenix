2a9d342fcb654b46162d5238797eb9c2d97a06ef
fenix 2.5 <hello@mail.com>
2019-05-16 20:28:06 +0100

    160519 work

---
+++ fenix/code/adc.py
@@ -0,0 +1,37 @@
+# Simple example of reading the MCP3008 analog input channels and printing
+# them all out.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+def convert_to_voltage(adc_value):
+    return round(3.3*adc_value/1024, 2)
+
+
+def single_battery_voltage(adc_value):
+    return round(1.5*convert_to_voltage(adc_value),2)
+
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+
+print('Reading MCP3008 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*range(8)))
+print('-' * 57)
+# Main program loop.
+while True:
+    # Read all the ADC channel values in a list.
+    values = [0]*8
+    for i in range(8):
+        # The read_adc function will get the value of the specified channel (0-7).
+        values[i] = mcp.read_adc(i)
+    rpi_battery_voltage = single_battery_voltage(values[0])
+    print(f'rpi_battery_voltage : {rpi_battery_voltage}')
+    time.sleep(0.5)
