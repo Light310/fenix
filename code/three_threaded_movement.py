1b62d6dd3a4554da371ffa3fed80bf15965b0647
fenix <fenix@nexus.com>
2019-10-13 18:21:13 +0100

    req file and movement fix

--- code/three_threaded_movement.py
+++ code/three_threaded_movement.py
@@ -5,16 +5,9 @@ import sys
 import time
 import argparse
 import pigpio
-sys.path.append('/nexus/fenix/')
-from common.BasicConfig import BasicConfig
 
-
-#cfg = BasicConfig()
-#sequence_file = cfg.sequence_file
-#sequence_file = 'C:\\Users\\Sergey\\PycharmProjects\\fenix\\wrk\\sequence.txt'
-#sequence_path = "D:\\Development\\Python\\"
-wrk_path = '/nexus/fenix/wrk/'
-sequence_path = '/nexus/fenix/wrk/sequences/'
+wrk_path = '/fenix/tmp/'
+sequence_path = '/fenix/static/sequences/'
 sequence_dict = {
     'Back': 'sq_backw_8',
     'LeftBackwards': 'sq_backw_left_5',
