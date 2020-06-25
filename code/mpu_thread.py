e1d1cc8295961dcb5443405ae1ab3eb4391e95a1
Sergey Kryukov <kryukovS310@gmail.com>
2020-06-25 17:30:16 +0100

    correct values for mpus

--- code/mpu_thread.py
+++ code/mpu_thread.py
@@ -10,39 +10,41 @@ import time
 # calibration values
 
 gyro_calibration = {
-    0  : [-105, 199, -184],
-    1  : [-348, -113, 131],
-    2  : [-406, 41, -39],
-    3  : [-118, 307, 41],
-    4  : [-77, -87, -483],
-    5  : [55, 61, -41],
-    6  : [-331, 51, 278],
-    7  : [-316, 304, 227],
-    8  : [-501, 204, -62],
-    9  : [-164, -52, -707],
-    10 : [-109, 110, -124],
-    11 : [-166, 201, 4],
-    12 : [-270, -905, 6],
-    13 : [0, 0, 0],
-    14 : [-426, -2339, 9]
+    0  : [-164, -52, -707],
+    1  : [-501, 204, -62],
+    2  : [-316, 304, 227],
+    3  : [-947, 434, -44],
+    4  : [-270, -905, 6],
+    5  : [-166, 201, 4],
+    6  : [-109, 110, -124],
+    7  : [326, -329, 136],
+    8  : [-118, 307, 41],
+    9  : [-406, 41, -39],
+    10 : [-348, -113, 131],
+    11 : [-105, 199, -184],
+    12 : [-331, 51, 278],
+    13 : [55, 61, -41],
+    14 : [-77, -87, -483],
+	15 : [-426, -2339, 9]
 }
 
 accel_calibration = {
-    0  : [1589.76, -1.99, -2447.02],
-    1  : [807.61, 296.6, 881.99],
-    2  : [10475.81, 271.38, 187.23],
-    3  : [475.85, 72.36, 130.25],
-    4  : [1613.6, -83.67, -352.14],
-    5  : [12087.16, -35.59, -2052.18],
-    6  : [-1.53, 51.99, -217.49],
-    7  : [3039.0, 135.8, 402.79],
-    8  : [627.59, -3.69, 2848.23],
-    9  : [477.5, -460.0, -3647.0],
-    10 : [1718.0, 111.71, -412.89],
-    11 : [536.01, -5.97, -2090.6],
-    12 : [-771.5, 594.58, 2592.4],
-    13 : [0, 0, 0],
-    14 : [5705.5, 77.5, 2356.0]
+    0  : [477.5, -460.0, -3647.0],
+    1  : [627.59, -3.69, 2848.23],
+    2  : [3039.0, 135.8, 402.79],
+    3  : [321.7, 237.13, -1168.41],
+    4  : [-771.5, 594.58, 2592.4],
+    5  : [536.01, -5.97, -2090.6],
+    6  : [1718.0, 111.71, -412.89],
+    7  : [502.81, 230.21, -1009.7],
+    8  : [475.85, 72.36, 130.25],
+    9  : [10475.81, 271.38, 187.23],
+    10 : [807.61, 296.6, 881.99],
+    11 : [1589.76, -1.99, -2447.02],
+    12 : [-1.53, 51.99, -217.49],
+    13 : [12087.16, -35.59, -2052.18],
+    14 : [1613.6, -83.67, -352.14],
+	15 : [5705.5, 77.5, 2356.0]
 }
 
 
@@ -59,7 +61,7 @@ class fenix_imus(threading.Thread):
         #mcp.set_a_values(0b11111111)
         #mcp.set_b_values(0b11111111)
 
-        for pin_number in [0, 4, 5, 6, 14]:
+        for pin_number in range(16):
         #for pin_number in [6, 13, 19, 26, 6, 13, 19, 26, 6, 13, 19, 26]:            
             self.imus.append(mpu6050(calibrate=False, 
                                 mcp=mcp,
@@ -114,11 +116,14 @@ class fenix_imus(threading.Thread):
     def get_leg_servo_angles(self):
         # returns 4 arrays of angles, each for the corresponding leg
         angles = self.real_angles
-        leg_2_angles = self.convert_mpus_to_servos([angles[4], angles[1], angles[2], angles[3]])
-        return [None, None, None, None,
-                *leg_2_angles,    None, 
-                None, None, None, None,
-                None, None, None, None]
+        leg_1_angles = self.convert_mpus_to_servos([angles[11], angles[10], angles[9], angles[8]])
+        leg_2_angles = self.convert_mpus_to_servos([angles[15], angles[14], angles[13], angles[12]])
+        leg_3_angles = self.convert_mpus_to_servos([angles[3], angles[2], angles[1], angles[0]])
+        leg_4_angles = self.convert_mpus_to_servos([angles[7], angles[6], angles[5], angles[4]])
+        return [*leg_1_angles, None,
+                *leg_2_angles, None, 
+                *leg_3_angles, None,
+                *leg_4_angles, None]
 
     
     def start(self):
@@ -156,7 +161,8 @@ while True:
     print('Servos : {0}'.format(imus.get_leg_servo_angles()))
     #print('planes: {0}'.format([round(joint1, 2), round(joint2, 2), round(joint3, 2)]))
     print('----------------------------------')
-    time.sleep(0.1)
+    time.sleep(0.01)
 print('End : {0}'.format(datetime.datetime.now()))
 imus.stop()
+
 """
